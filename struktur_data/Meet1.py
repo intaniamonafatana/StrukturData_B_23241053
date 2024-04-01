389117dc85549a3daf66c63c81dac763150adf79
intaniamonafatana <146052216+intaniamonafatana@users.noreply.github.com>
2024-04-01 15:29:22 +0700

    Add files via upload

---
+++ struktur_data/Meet1.py
@@ -0,0 +1,5 @@
+#Perkenalkan Diri Nata Kuliah StrukturData
+print("NAMA SAYA intan") #Saya Sangat Ceria
+print("NIM 23241053")
+print("Saya Adalah Mahasiswa UNDIKMA PRODI PTI")
+#Saya Sangat Ceria
---
+++ struktur_data/Meet2.py
@@ -0,0 +1,20 @@
+# belajar variabel
+#assigment
+x = 3
+y = 7
+uang = 10000
+
+# pemanggilan
+print("Nilai UAS Saya Adalah", x)
+print("Nilai UTS Saya Adalah", y)
+print("Nilai Sisa Uang Saya Adalah", uang)
+
+# penamaan variabel
+nilai_a = 9 # tidak boleh spasi 
+kotak10 = 10 # tidak boleh diawali angka 
+
+# imput variabel kedua 
+x = 70
+print("Nilai UAS Saya Adalah", x)
+x = 93
+print("Nilai UAS Saya Adalah", x)

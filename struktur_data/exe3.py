4bb1e919d303f623b1432f36d2a6b7624e26003f
intaniamonafatana <146052216+intaniamonafatana@users.noreply.github.com>
2024-04-22 20:49:37 +0700

    Add files via upload

---
+++ struktur_data/exe3.py
@@ -0,0 +1,21 @@
+# contoh tipe  data (integer) : angka satuan bulat
+bil_integer = 10001
+print (type (bil_integer))
+print ("type data dari bil_integer adalah : ", type(bil_integer))
+
+# contoh tipe  data (float) : bilangan desimal
+bil_float = 2.5
+print("type data bil_float adalah: ", type(bil_float))
+
+# contoh tipe  data (string) : kumpulan karekter
+bil_string = "putu"
+print("type data bil_string adalah: ", type (bil_string))
+
+# contoh tipe  data (bolean) : biner , 0,1 true false
+bil_bol = True 
+print("type data bil_bolean adalah: ", type (bil_bol))
+
+# contoh tipe  data dari bahasa C  
+from ctypes import c_double
+bil_double = c_double(2000000)
+print("type data bil_double adalah: ", type (bil_double))

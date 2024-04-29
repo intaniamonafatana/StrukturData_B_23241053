27fee8645efb8ffeff126fbea315f649afc58ee2
intaniamonafatana <146052216+intaniamonafatana@users.noreply.github.com>
2024-04-29 11:29:03 +0700

    Add files via upload

---
+++ struktur_data/meet4&5.py
@@ -0,0 +1,52 @@
+# infut dari user 
+# aritmatika 
+
+# belajar infutan
+intan = input("masukan kata : ")
+print("isi dari intan: ", intan,"bertipe data :",type (intan))
+
+# belajar aritmatika dasar 
+x = 4
+y = 9
+
+# perjumlahan + 
+hasil = x + y 
+print("x + y =", hasil) 
+
+# pengurangan -
+hasil = x - y
+print("x - y =", hasil)
+
+# perkalian *
+hasil = x * y
+print("x * y =", hasil)
+
+# pembagian /
+hasil = x / y
+print("x / y =", hasil)
+
+# perpangkatan exponen **
+hasil = x ** y 
+print("x ** y =", hasil)
+
+# modulus%
+hasil = y % x
+print("x mood y =", hasil)
+
+#floor division (pembulatan kebawah)
+hasil = x // y 
+print("x // y =", hasil)
+
+
+#aritmatika prioritas
+# (), exponen, perkalian / pembagian, pejumlahan/ pengurangan 
+x = 3 
+y = 4
+z = 5
+
+hasil = (x ** y *(z + x ) / y - z % x // y )
+print(hasil)
+
+
+
+
